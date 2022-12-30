257c2683d378f41391db84c0a6cc56cc7f9fc6c0
Supanut1911 <ynotmela@gmail.com>
2022-12-30 09:54:32 +0700

    🔧 [chore] - add authModule-userModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { configService } from './config/config-service';
 import { TodoModule } from './todo/todo.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     TodoModule,
+    AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
 })
